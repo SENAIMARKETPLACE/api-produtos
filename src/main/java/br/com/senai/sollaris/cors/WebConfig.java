85940513a5f7036acf10ad65dfe2a1d4fcedaa6b
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-03-30 15:45:58 -0300

    Feature: CORS Created

---
+++ src/main/java/br/com/senai/sollaris/cors/WebConfig.java
@@ -0,0 +1,22 @@
+package br.com.senai.sollaris.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	
+	// * SIGNIFICA QUE TUDO ESTÁ LIBERADO
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("localhost:3000")
+		.allowedOrigins("*")
+		.allowedHeaders("*")
+		.allowedMethods("GET","POST","PUT","DELETE");
+		
+	}
+
+}
