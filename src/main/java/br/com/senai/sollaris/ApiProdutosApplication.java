9ebcb45afecd8578b0ed9228ddb4054b83f050c2
Ddalmeida1 <eduardoalmeida4k@gmail.com>
2023-04-19 21:04:46 -0300

    Criação da Documentação Swagger

--- src/main/java/br/com/senai/sollaris/ApiProdutosApplication.java
+++ src/main/java/br/com/senai/sollaris/ApiProdutosApplication.java
@@ -4,8 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "API de Produto",
+	version = "2.7.9", description = "CRUD para manter produto",
+	license = @License(name = "Pertencente: Sollaris Marketplace", url = "https://github.com/SENAIMARKETPLACE"),
+	contact = @Contact(name = "Email da Empresa", email = "sollaris@gmail.com.br"),
+	termsOfService = "Terms_Service"))
 public class ApiProdutosApplication {
 
 	public static void main(String[] args) {
